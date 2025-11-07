a69311912d871a1ed9e6532e3ceaab9de5a143a4
Sara Fernandez <sarafdez2001@gmail.com>
2025-11-07 13:53:38 +0100

    Creación de proyecto versión inicial

---
+++ src/main/java/com/mycompany/actividadpractiva/Main.java
@@ -0,0 +1,12 @@
+package com.mycompany.actividadpractiva;
+
+/**
+ *
+ * @author Sara Fernández
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Versión inicial del proyecto.");
+    }
+}
