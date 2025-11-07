fe9ec62a22ca34fe246e819cd23398991043207f
Sara Fernandez <sarafdez2001@gmail.com>
2025-11-07 13:58:59 +0100

    Funcionalidad de login implementada

--- src/main/java/com/mycompany/actividadpractiva/Main.java
+++ src/main/java/com/mycompany/actividadpractiva/Main.java
@@ -8,5 +8,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Versión inicial del proyecto.");
+        System.out.println("Funcionalidad de login implementada.");
     }
 }
